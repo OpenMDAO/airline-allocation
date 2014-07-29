f3a9539db30716855a39c2c6ddf48a9e9a539438
unknown <crrobin3@GRSDW40036980.ndc.nasa.gov>
2014-07-29 10:49:25 -0400

    Changed repository to OpenMDAO plugin

---
+++ airline_alloc/docs/index.rst
@@ -0,0 +1,13 @@
+===========================
+airline_alloc Documentation
+===========================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+    
+   usage
+   srcdocs
+   pkgdocs
+
---
+++ airline_alloc/src/airline_alloc/test/test_airline_alloc.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+
+class Airline_allocTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    # add some tests here...
+    
+    #def test_Airline_alloc(self):
+        #pass
+        
+if __name__ == "__main__":
+    unittest.main()
+    
