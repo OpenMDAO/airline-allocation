7a449900774fcd47e8faa4b7b0ec6b28e449d500
Stephen Ryan <stephen.w.ryan@gmail.com>
2014-08-06 12:33:14 -0400

    first hack at branch_cut functions

--- src/airline_alloc/test/test_swryan.py
+++ src/airline_alloc/test/test_swryan.py
@@ -123,6 +123,260 @@ def get_constraints(inputs, constants, coefficients):
     return A, b
 
 
+def gomory_cut(x, A, b, Aeq, beq):
+    num_des = len(x)
+    slack = []
+    if len(b) > 0:
+        slack = b - A * x
+        x_up = [[x], [slack]]
+        Ain_com = [A, np.eye(len(slack))]
+    else:
+        x_up = x.copy()
+        Ain_com = []
+
+    if len(beq) > 0:
+        Aeq_com = [Aeq, np.zeros((Aeq.shape[0], len(slack)))]
+    else:
+        Aeq_com = []
+
+    Acom = [[Ain_com], [Aeq_com]]
+    bcom = [[b], [beq]]
+
+    # Generate the Simplex optimal tableau
+    aaa = np.nonzeros((x_up - 0) > 1e-06)
+    B = []
+    for ii in range(len(aaa)):
+        B = [B, Acom[:, aaa[ii]]]
+    tab = [np.linalg.solve(B, Acom), np.linalg.solve(B, bcom)]
+
+    # Generate cut
+    # Select the row from the optimal tableau corresponding
+    # to the basic design variable that has the highest fractional part
+    b_end = tab[:, tab.shape[0]]
+
+    aa = np.nonzeros(abs(round(b_end) - b_end) > 1e-06)
+    __, rw_sel = max(np.remainder(abs(tab[aa, :]), 1))
+
+    eflag = 0
+
+    if len(rw_sel) > 0:
+        # apply Gomory cut
+        equ_cut = tab[rw_sel, :]
+        lhs = np.floor(equ_cut)
+        rhs = -(equ_cut - lhs)
+        lhs[-1] = -lhs[-1]
+        rhs[-1] = -rhs[-1]
+
+        # cut: rhs < 0
+        a_x = rhs[1:num_des]
+        a_s = rhs[num_des + 1:rhs.shape[0] - 1]
+        A_new = a_x - a_s * A
+        b_new = - (rhs[rhs.shape[0]] + a_s * b)
+
+        aa = np.nonzeros(abs(A_new - 0) <= 1e-08)
+        A_new[aa] = 0
+        bb = np.nonzeros(abs(b_new - 0) <= 1e-08)
+        b_new[bb] = 0
+
+        # Update and print cut information
+        if (np.sum(A_new) != 0) and (np.sum(np.isnan(A_new)) == 0):
+            eflag = 1
+            A_up = [[A], [A_new]]
+            b_up = [[b], [b_new]]
+
+            cut_stat = ''
+            for ii in range(len(A_new) + 1):
+                if ii == len(A_new) + 1:
+                    symbol = ' <= '
+                    cut_stat = cut_stat + symbol + str(b_new[b_new.shape[0]])
+                    break
+                if A_new[ii] != 0:
+                    if A_new[ii] < 0:
+                        symbol = ' - '
+                    else:
+                        if len(cut_stat) > 0:
+                            symbol = ''
+                        else:
+                            symbol = ' + '
+                    cut_stat = cut_stat + symbol + str(abs(A_new[ii])) + 'x', str(ii)
+
+    if eflag == 1:
+        print('\\n%s\\n', 'Applying cut: ', cut_stat)
+    else:
+        A_up = A.copy()
+        b_up = b.copy()
+        print('\\n%s\\n', 'No cut applied!!')
+
+    return A_up, b_up, eflag
+
+
+def cut_plane(x, A, b, Aeq, beq, ind_con, ind_int, indeq_con, indeq_int, num_int):
+    num_con = len(x) - num_int
+    x_trip = x[1:num_int]
+    pax = x[num_int + 1:x.shape[0]]
+
+    if len(b) > 0:
+        A_x_int   = A[ind_int, 1:num_int]
+        A_pax_int = A[ind_int, num_int + 1:A.shape[1]]
+        b_x_int   = b[ind_int] - A_pax_int * pax
+    else:
+        A_x_int = []
+        b_x_int = []
+
+    if len(beq) > 0:
+        Aeq_x_int   = Aeq[indeq_int, 1:num_int]
+        Aeq_pax_int = Aeq[indeq_int, num_int + 1:Aeq.shape[1]]
+        beq_x_int   = beq[indeq_int] - Aeq_pax_int * pax
+    else:
+        Aeq_x_int = []
+        beq_x_int = []
+
+    A_x_int_up, b_x_int_up, eflag = gomory_cut(x_trip, A_x_int, b_x_int, Aeq_x_int, beq_x_int)
+    if eflag == 1:
+        A_new = [A_x_int_up[A_x_int_up.shape[0], :], np.ones(1, num_con)]
+        b_new = b_x_int_up[b_x_int_up.shape[0]] + np.ones(1, num_con) * pax
+    else:
+        A_new = []
+        b_new = []
+
+    A_up = np.concatenate(A, A_new)
+    b_up = np.concatenate(b, b_new)
+    return A_up, b_up
+
+
+def branch_cut(f_int, f_con, A, b, Aeq, beq, lb, ub, x0,
+               ind_conCon, ind_intCon, indeq_conCon, indeq_intCon):
+    """ branch and bound algorithm
+    """
+    f = [[f_int], [f_con]]
+    num_int = len(f_int)
+    num_con = len(f_con)
+    num_des = num_int + num_con
+
+    _iter = 0
+    funCall = 0
+    eflag = 0
+    U_best = np.inf
+    xopt = []
+    fopt = []
+    can_x = []
+    can_F = []
+    ter_crit = 0
+    opt_cr = 0.03
+    node_num = 1
+    tree = 1
+
+    Aset = object()
+    Aset.f = f
+    Aset.A = A
+    Aset.b = b
+    Aset.Aeq = Aeq
+    Aset.beq = beq
+    Aset.lb = lb
+    Aset.ub = ub
+    Aset.x0 = x0
+    Aset.b_F = 0
+    Aset.x_F = []
+    Aset.node = node_num
+    Aset.tree = tree
+
+    while len(Aset) > 0 and ter_crit != 2:
+
+        _iter = _iter + 1
+
+        # pick a subproblem
+        # preference given to nodes with higher objective value
+        Fsub = -np.inf
+        for ii in range(len(Aset)):
+            if Aset[ii].b_F >= Fsub:
+                Fsub_i = ii
+                Fsub = Aset[ii].b_F
+
+        # solve subproblem using linprog
+        Aset[Fsub_i].x_F, Aset[Fsub_i].b_F, Aset[Fsub_i].eflag = \
+            np.linprog(Aset[Fsub_i].f,
+                       Aset[Fsub_i].A, Aset[Fsub_i].b,
+                       Aset[Fsub_i].Aeq, Aset[Fsub_i].beq,
+                       Aset[Fsub_i].lb, Aset[Fsub_i].ub,
+                       Aset[Fsub_i].x0)
+
+        funCall = funCall + 1
+
+        # rounding integers
+        aa = np.nonzeros(abs(round(Aset[Fsub_i].x_F) - Aset[Fsub_i].x_F) <= 1e-06)
+        Aset[Fsub_i].x_F[aa] = round(Aset[Fsub_i].x_F(aa))
+
+        if _iter == 1:
+            x_best_relax = Aset[Fsub_i].x_F
+            f_best_relax = Aset[Fsub_i].b_F
+
+        if ((Aset[Fsub_i].eflag >= 1) and (Aset[Fsub_i].b_F < U_best)):
+            if np.norm(Aset[Fsub_i].x_F(range(num_int)) - round(Aset[Fsub_i].x_F(range(num_int)))) <= 1e-06:
+                can_x = [can_x, Aset[Fsub_i].x_F]
+                can_F = [can_F, Aset[Fsub_i].b_F]
+                x_best = Aset[Fsub_i].x_F
+                U_best = Aset[Fsub_i].b_F
+                print '\n%s',    '======================='
+                print '\n%s',    'New solution found!'
+                print '\n%s\n', '======================='
+                Aset[Fsub_i] = []  # Fathom by integrality
+                ter_crit = 1
+                if (abs(U_best - f_best_relax) / abs(f_best_relax)) <= opt_cr:
+                    ter_crit = 2
+            else:  # cut and branch
+
+                # apply cut to subproblem
+                if Aset[Fsub_i].node != 1:
+                    Aset[Fsub_i].A, Aset[Fsub_i].b = cut_plane(Aset[Fsub_i].x_F,
+                        Aset[Fsub_i].A, Aset[Fsub_i].b,
+                        Aset[Fsub_i].Aeq, Aset[Fsub_i].beq,
+                        ind_conCon, ind_intCon,
+                        indeq_conCon, indeq_intCon,
+                        num_int)
+
+                # branching
+                __, x_ind_maxfrac = max(np.remainder(abs(Aset[Fsub_i].x_F(range(num_int))), 1))
+                x_split = Aset[Fsub_i].x_F(x_ind_maxfrac)
+                print '\n%s%d%s%d%s%f\n' % ('Branching at tree: ', Aset[Fsub_i].tree, ' at x', x_ind_maxfrac, ' = ', x_split)
+                F_sub = []
+                for jj in range(2):
+                    F_sub[jj] = Aset[Fsub_i]
+                    A_rw_add = np.zeros(1, len(Aset[Fsub_i].x_F))
+                    if jj == 0:
+                        A_con = 1
+                        b_con = np.floor(x_split)
+                    else:
+                        if jj == 2:
+                            A_con = -1
+                            b_con = -np.ceil(x_split)
+
+                    A_rw_add[x_ind_maxfrac] = A_con
+                    A_up = [[F_sub[jj].A], [A_rw_add]]
+                    b_up = [[F_sub[jj].b], [b_con]]
+                    F_sub[jj].A = A_up
+                    F_sub[jj].b = b_up
+                    F_sub[jj].tree = 10 * F_sub[jj].tree + jj
+                    node_num = node_num + 1
+                    F_sub[jj].node = node_num
+                Aset[Fsub_i] = []
+                Aset = [Aset, F_sub]
+        else:
+            Aset[Fsub_i] = []  # Fathomed by infeasibility or bounds
+
+    if ter_crit > 0:
+        eflag = 1
+        xopt = x_best.copy()
+        fopt = U_best.copy()
+        if ter_crit == 1:
+            print '\n%s%0.1f%s\n' % ('Solution found but is not within ', opt_cr*100, '%% of the best relaxed solution!')
+        elif ter_crit == 2:
+            print '\n%s%0.1f%s\n' % ('Solution found and is within ', opt_cr*100, '%% of the best relaxed solution!')
+    else:
+        print '\n%s\n' % 'No solution found!!'
+
+    return xopt, fopt, can_x, can_F, x_best_relax, f_best_relax, funCall, eflag
+
+
 class RangeExtractTestCase(unittest.TestCase):
 
     def test_3routes(self):
@@ -233,6 +487,47 @@ class ConstraintsTestCase(unittest.TestCase):
 
         self.assertTrue(np.allclose(A, expected_A))
         self.assertTrue(np.allclose(b, expected_b))
+
+
+class MainTestCase(unittest.TestCase):
+
+    def test_branch_cut(self):
+        # smaller network with 3 routes
+        inputs       = load_data_file('inputs_after_3routes.mat')['Inputs']
+        outputs      = load_data_file('outputs_after_3routes.mat')['Outputs']
+        constants    = load_data_file('constants_after_3routes.mat')['Constants']
+        coefficients = load_data_file('coefficients_after_3routes.mat')['Coefficients']
+
+        # linear objective coefficients
+        objective   = get_objective(inputs, outputs, constants, coefficients)
+        f_int = objective[0]    # integer type design variables
+        f_con = objective[1]    # continuous type design variables
+
+        # coefficient matrix for linear inequality constraints, Ax <= b
+        constraints = get_constraints(inputs, constants, coefficients)
+        A = constraints[0]
+        b = constraints[1]
+
+        J = inputs.DVector.shape[0]  # number of routes
+        K = len(inputs.AvailPax)     # number of aircraft types
+
+        # lower and upper bounds
+        lb = np.zeros((2*K*J, 1))
+        ub = np.concatenate((
+            np.ones((K*J, 1)) * inputs.MaxTrip.reshape(-1, 1),
+            np.ones((K*J, 1)) * np.inf
+        ))
+
+        # initial x
+        x0 = []
+
+        # indices into A matrix for continuous & integer/continuous variables
+        ind_conCon = range(2*J)
+        ind_intCon = range(2*J, len(constraints[0])+1)
+
+        # call the branch and cut algorithm to solve the MILP problem
+        branch_cut(f_int, f_con, A, b, [], [], lb, ub, x0,
+                   ind_conCon, ind_intCon, [], [])
 
 
 if __name__ == "__main__":
