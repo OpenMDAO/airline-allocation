cbc9dd73c79a7831b28f4588de82b704858b1632
swryan <stephen.w.ryan@gmail.com>
2014-08-10 18:45:52 -0400

    simple wrap of scipy.optimize.linprog as openmdao component

--- src/airline_alloc/LinearProblem.py
+++ src/airline_alloc/LinearProblem.py
@@ -1,7 +1,5 @@
 
-from openmdao.main.api import Component, Array, Bool
-
-import numpy as np
+from openmdao.main.api import Component, Array, Bool, Int
 
 try:
     from scipy.optimize import linprog
@@ -13,35 +11,52 @@ except ImportError, e:
 class LinearProblem(Component):
 
     # inputs
-    f     = Array(iotype='in', desc='objective function')
+    c     = Array(iotype='in',
+            desc='coefficients of the linear objective function to be maximized')
 
-    A     = Array(iotype='in', desc='inequality constraints, coefficients matrix')
-    b     = Array(iotype='in', desc='inequality constraints')
+    A_ub  = Array(iotype='in',
+            desc='2-D array which, when matrix-multiplied by x, gives the values of the upper-bound inequality constraints at x')
 
-    Aeq   = Array(iotype='in', desc='equality constraints, coefficients matrix')
-    beq   = Array(iotype='in', desc='equality constraints')
+    b_ub  = Array(iotype='in',
+            desc='1-D array of values representing the upper-bound of each inequality constraint (row) in A_ub')
 
-    lb    = Array(iotype='in', desc='lower bounds')
-    ub    = Array(iotype='in', desc='upper bounds')
+    A_eq  = Array(iotype='in',
+            desc='2-D array which, when matrix-multiplied by x, gives the values of the equality constraints at x')
+
+    b_eq  = Array(iotype='in',
+            desc='1-D array of values representing the RHS of each equality constraint (row) in A_eq')
+
+    lb    = Array(iotype='in',
+            desc='lower bounds for each independent variable in the solution')
+
+    ub    = Array(iotype='in',
+            desc='upper bounds for each independent variable in the solution')
 
     # outputs
-    x_F   = Array(iotype='out', desc='')
-    b_F   = Array(iotype='out', desc='')
+    x     = Array(iotype='out',
+            desc='independent variable vector which optimizes the linear programming problem')
 
-    eflag = Bool(iotype='out', desc='flag indicating success or failure in finding a solution')
+    fun   = Array(iotype='out',
+            desc='function value')
+
+    success = Bool(iotype='out',
+              desc='flag indicating success or failure in finding an optimal solution')
+
+    status  = Int(iotype='out',
+              desc='exit status of the optimization: 0=optimized, 1=max iterations, 2=infeasible, 3=unbounded')
 
     def execute(self):
-        """ solve subproblem using linprog
+        """ solve problem
         """
-        bounds = zip(self.lb, self.ub)
 
-        results = linprog(self.f,
-                          A_eq=None,   b_eq=None,
-                          A_ub=self.A, b_ub=self.b,
-                          bounds=bounds,
+        results = linprog(self.c,
+                          A_eq=self.A_eq,   b_eq=self.b_eq,
+                          A_ub=self.A_ub,   b_ub=self.b_ub,
+                          bounds=zip(self.lb, self.ub),
                           options={ 'maxiter': 100, 'disp': True })
 
-        print 'results:\n---------------\n', results, '\n---------------'
-        self.x_F = results.x
-        self.b_F = results.fun
-        self.eflag = 1 if results.success else 0
+        print self.get_pathname(), 'results:\n---------------\n', results, '\n---------------'
+        self.x   = results.x
+        self.fun = results.fun
+        self.success = results.success
+        self.status  = results.status
